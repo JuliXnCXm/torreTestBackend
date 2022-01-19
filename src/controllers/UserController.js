626032c340c42273bc1e01fd38607c0e7a0336ca
julian garcia <juliancamilogarciaescobar7@gmail.com>
2022-01-19 15:40:09 -0500

    login and resgiter methods create

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const axios = require('axios');
 const { config } = require( '../config/config' );
-
+const User = require('../models/User');
+const mongoose = require('mongoose');
 class UserController {
     getUser = (req, res) => {
         axios.get(`${config.apiUserTest}/${req.params.username} `)
@@ -15,8 +16,33 @@ class UserController {
 
     login = (req, res) => {
         let {email , password} = req.body
-        let decode = jwt.decode(token, config.privateKey);
+        User.findOne({email: email, password: password}, (err, user) => {
+            if (err) {
+                res.status(500).send({info: error});
+            } else {
+                if (user != null && user != undefined) {
+                    let token = jwt.sign({user}, config.privateKey);
+                    res.status(200).send({token});
+                } else {
+                    res.status(404).send({info: 'User not found'});
+                }
+            }
+        })
     }
-    register = (req, res) => {}
+    register = (req, res) => {
+        let objUser = req.body
+        if (objUser.name && objUser.userlastname && objUser.email && objUser.password) {
+            User.create(objUser, (err, user) => {
+                if(!err) {
+                    let token = jwt.sign({user}, config.privateKey);
+                    res.status(201).send({token});
+                } else {
+                    res.status(500).send({info: err});
+                }
+            })
+        } else {
+            res.status(500).send({info: 'Error'});
+        }
+    }
 }
 module.exports = UserController;
