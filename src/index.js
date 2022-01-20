d14c76fd43988412be26e32801a9fa4dddb72e7c
julian garcia <juliancamilogarciaescobar7@gmail.com>
2022-01-20 11:54:24 -0500

    final commit

--- src/index.js
+++ src/index.js
@@ -26,6 +26,9 @@ class Server {
         this.app.use('/', IndexR.router);
         this.app.use('/user', UserR.router);
 
+        this.app.listen(process.env.PORT || 5000, () => {
+            console.log("Server on port", this.app.get("port"));
+        });
     }
 
 }
